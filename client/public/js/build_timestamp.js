bc1b424154713e6e4e9d87e48e5b947b60a6ff46
Michael Stauffer <mgstauff@gmail.com>
2022-08-04 19:28:56 -0400

    Build timestamp - switch to UTC time

--- client/public/js/build_timestamp.js
+++
@@ -1,2 +0,0 @@
-//Genereated at build time via package.json and local script D:\Documents\Vizmoo-local\GC\Dev\GC-RCApp\project\generate-build-timestamp.sh
-export const BUILD_TIMESTAMP = "2022-08-01-22-26"
--- generate-build-timestamp.sh
+++ generate-build-timestamp.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #Generate a simple timestamp file that gets picked up in subsequent
 # build step to get included in the web app
+#Use UTC time so it's always the same between local and AWS server
 FIL="client/public/js/build_timestamp.js"
 echo Generating timestamp $FIL
 echo "//Genereated at build time via package.json and local script $0" > $FIL
-echo export const BUILD_TIMESTAMP = \"`date +%F-%H-%M`\" >> $FIL
+echo export const BUILD_TIMESTAMP = \"`date -u +%F-%H-%M`\" >> $FIL
